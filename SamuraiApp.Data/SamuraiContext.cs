ccdda57122f45b8fcabb01235471f81ad6d896e6
Gabriel Girbacea <gabriel.girbacea@gmail.com>
2021-02-03 17:04:07 +0200

    Created SamuraiContext and added EntityFrameworkCore package.
    Created SamuraiContext and added EntityFrameworkCore package.

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
+        }
+    }
+}
