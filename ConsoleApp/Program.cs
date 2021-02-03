20b14e23e6617790fb83d957a0a24b396f9b1e27
Gabriel Girbacea <gabriel.girbacea@gmail.com>
2021-02-03 19:55:08 +0200

    Added simple operations to ConsoleApp to play with EF.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using SamuraiApp.Data;
+using SamuraiApp.Domain;
+using System;
+using System.Linq;
 
 namespace ConsoleApp
 {
-    class Program
+    internal class Program
     {
+        private static SamuraiContext context = new SamuraiContext();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            context.Database.EnsureCreated();
+            GetSamurais("Before Add");
+            AddSamurai();
+            GetSamurais("After Add");
+            Console.Write("Press any key...");
+            Console.ReadKey();
+        }
+
+        private static void AddSamurai()
+        {
+            var samurai = new Samurai()
+            {
+                Name = "Julie"
+            };
+
+            context.Samurais.Add(samurai);
+            context.SaveChanges();
+        }
+
+        private static void GetSamurais(string text)
+        {
+            var samurais = context.Samurais.ToList();
+            Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+            foreach (var samurai in samurais)
+            {
+                Console.WriteLine(samurai.Name);
+            }
         }
     }
 }
