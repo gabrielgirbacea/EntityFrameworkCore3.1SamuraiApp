913fd2c248be390d036d39c57d350c07414aaeaf
Gabriel Girbacea <gabriel.girbacea@gmail.com>
2021-02-04 23:18:54 +0200

    Added required changes to make the application run as expected.

--- SamuraiAPI/Startup.cs
+++ SamuraiAPI/Startup.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SamuraiApp.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,11 @@ namespace SamuraiAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SamuraiContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("SamuraiConnex"))
+                       .EnableSensitiveDataLogging();
+            });
             services.AddControllers();
         }
 
