51d6b3fda9f79e53ce1f7db322e1c47cd012c5cb
Gabriel Girbacea <gabriel.girbacea@gmail.com>
2021-02-04 13:57:07 +0200

    Created an emtpy migration and populated it with raw sql code that will create a function and a view to display function's results along with other data.
    Created an emtpy migration and populated it with raw sql code that will create a function and a view to display function's results along with other data.

---
+++ SamuraiApp.Data/Migrations/20210204115155_SamuraiBattleStats.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SamuraiApp.Data.Migrations
+{
+    public partial class SamuraiBattleStats : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"CREATE FUNCTION[dbo].[EarliestBattleFoughtBySamurai](@samuraiId int)
+                   RETURNS char(30) AS
+                   BEGIN
+                     DECLARE @ret char(30)
+                     SELECT TOP 1 @ret = Name
+                     FROM Battles
+                     WHERE Battles.Id IN(SELECT BattleId
+                                        FROM SamuraiBattle
+                                       WHERE SamuraiId = 2)
+                     ORDER BY StartDate
+                     RETURN @ret
+                   END");
+
+            migrationBuilder.Sql(
+                @"CREATE OR ALTER VIEW dbo.SamuraiBattleStats
+                  AS
+                  SELECT dbo.SamuraiBattle.SamuraiId, dbo.Samurais.Name,
+                  COUNT(dbo.SamuraiBattle.BattleId) AS NumberOfBattles,
+                          dbo.EarliestBattleFoughtBySamurai(MIN(dbo.Samurais.Id)) AS EarliestBattle
+                  FROM dbo.SamuraiBattle INNER JOIN
+                       dbo.Samurais ON dbo.SamuraiBattle.SamuraiId = dbo.Samurais.Id
+                  GROUP BY dbo.Samurais.Name, dbo.SamuraiBattle.SamuraiId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP VIEW dbo.SamuraiBattleStats");
+            migrationBuilder.Sql("DROP FUNCTION dbo.EarliestBattleFoughtBySamurai");
+        }
+    }
+}
